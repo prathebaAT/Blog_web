a21acc3fa59d1b91ed14b43ce19c9e649be0d4de
prathebaAT <pratheba63@gmail.com>
2024-06-30 01:41:36 +0530

    updated files for user registeration and login

--- blogpage/urls.py
+++ blogpage/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('', views.register, name='register'),
+    path('blogposts', views.post_list, name='post_list'),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
+    
 ]
--- blogpage/views.py
+++ blogpage/views.py
@@ -1,10 +1,41 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User , auth
+from django.contrib import messages
 from .models import Post
+
 
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email= request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Already Used')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Already Used')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save();
+                return redirect('login')
+        else:
+            messages.info(request, 'Password Not the same')
+            return redirect('register')
+    
+
     return render(request, 'blog/register.html')
+
+def login(request):
+    if request.method == 'POST':
+        username
+
+
