080cae8574a29917d8b44546d5271608e1a8bb35
prathebaAT <pratheba63@gmail.com>
2024-06-29 17:15:11 +0530

    updated register function

--- blogpage/views.py
+++ blogpage/views.py
@@ -5,3 +5,6 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def register(request):
+    return render(request, 'blog/register.html')
